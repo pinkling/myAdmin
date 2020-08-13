509cad5d0b91bab73b0f041d4a618e5bbd911204
ling.yuan <yuanling@topevery.club>
2020-08-13 15:43:48 +0800

    背景添加

--- src/views/ling/loadingCycle.vue
+++ src/views/ling/loadingCycle.vue
@@ -1,12 +1,22 @@
 <template>
-  <section>
-    loadingCycle
+  <section style="position:relative;">
+    <canvas id="canvas" style="width:100%;height:calc(100vh - 84px);z-index: 1; opacity: 1;position:absolute;background: #588598; "></canvas>
   </section>
 </template>
 
 <script>
+import { init, onresize } from '@/plugin/background/draw'
 export default {
-  name: 'LoadingCycle'
+  name: 'LoadingCycle',
+  mounted() {
+    this.initBg()
+  },
+  methods: {
+    initBg() {
+      init()
+      window.addEventListener('resize', onresize)
+    }
+  }
 }
 </script>
 
